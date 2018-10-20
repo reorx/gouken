54a5a5bccf7d72e229593f2cc685026494fe1497
Reorx <novoreorx@gmail.com>
2018-10-20 17:06:05 +0800

    update examples

--- examples/poll/cmd/rpcserver/main.go
+++ examples/poll/cmd/rpcserver/main.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"time"
 
+	pb "github.com/reorx/gouken/examples/poll/proto"
+	"github.com/reorx/gouken/utils"
+
 	"github.com/reorx/gouken/examples/poll"
 )
 
@@ -16,6 +19,13 @@ func main() {
 		return nil
 	})
 	go app.GApp.MustRun()
+	go func() {
+		c := app.GRPCClient()
+		resp, err := c.AddCandidate(utils.Context(), &pb.AddCandidateRequest{
+			Name: "foo",
+		})
+		log.Printf("got response=%s, err=%v", resp, err)
+	}()
 
 	time.Sleep(time.Duration(3) * time.Second)
 	app.GApp.Stop()
